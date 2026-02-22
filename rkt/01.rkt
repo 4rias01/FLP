dc23fec56eb75cf62ba10b252ac097cfbe44a4ce
4rias01 <santiago.arias.rojas@correounivalle.edu.co>
2026-02-22 11:47:29 -0500

    primer punto parcialmente implementado

--- rkt/01.rkt
+++ rkt/01.rkt
@@ -1,1 +1,11 @@
 #lang eopl
+
+(define invert
+  (lambda (L P)
+    (cond
+      ((eqv? L empty) empty)
+      ((and (P (caar L)) (P (cadr (car L)))) (cons (car L) (invert (cdr L) P)))
+      (else invert (cdr L) P)
+      )
+    )
+  )
