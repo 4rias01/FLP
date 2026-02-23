66853916be05a7428814f2ef206ecfd278730e3e
4rias01 <santiago.arias.rojas@correounivalle.edu.co>
2026-02-22 20:43:46 -0500

    Primer punto hecho (no habia guardado el archivo)

--- rkt/01.rkt
+++ rkt/01.rkt
@@ -1,11 +1,23 @@
 #lang eopl
 
+(define cumplen
+  (lambda (pareja predicado)
+    (and (predicado (car pareja)) (predicado (cadr pareja)))
+    )
+  )
+
+(define cambiar
+  (lambda (pareja)
+    (cons (cadr pareja)
+          (cons (car pareja) '()))))
+
 (define invert
   (lambda (L P)
     (cond
-      ((eqv? L empty) empty)
-      ((and (P (caar L)) (P (cadr (car L)))) (cons (car L) (invert (cdr L) P)))
-      (else invert (cdr L) P)
+      [(eqv? L empty) empty]
+      [(cumplen (car L) P)
+       (cons (cambiar (car L)) (invert (cdr L) P))]
+      [else (invert (cdr L) P)]
       )
     )
   )
