90bfb7fe5502c6333a20020820082e265dbd3e02
TofiTurtle <juan.rodriguez.lozano@correounivalle.edu.co>
2026-02-24 23:15:37 -0500

    punto 10 parcialmente implementado, problemas a la hora de manipular los parentesis, de momento toca pasar como char, logicamente parece funcionar correctamente

---
+++ rkt/10.rkt
@@ -0,0 +1,25 @@
+#lang eopl
+
+;Taller FLP: Punto 10*
+
+;Funciones Auxiliares--------------------------------------------------------------
+
+
+
+;Fin Funciones Auxiliares----------------------------------------------------------
+(define auxParenth
+  (lambda (list acum)
+    (cond
+      ( (< acum 0) #f ) 
+      ( (null? list) (= acum 0) ) 
+      ( (eqv? (car list) #\( ) (auxParenth (cdr list) (+ acum 1)))
+      ( (eqv? (car list) #\) ) (auxParenth (cdr list) (- acum 1)))    
+      (else #f))))
+
+(define balanced-parentheses?
+  (lambda (L)
+    (auxParenth L 0)
+    ))
+
+
+;; Pruebas :
