963dcf217b607028b87e2df79e5e08d95d9d6a23
TofiTurtle <juan.rodriguez.lozano@correounivalle.edu.co>
2026-02-25 15:51:14 -0500

    codigo Funcion 12 implementado, falta gramatica + pruebas

--- rkt/11.rkt
+++ rkt/11.rkt
@@ -2,6 +2,9 @@
 
 ;Taller FLP: Punto 11*
 
+;;
+;;
+
 (define zip
   (lambda (F L1 L2)
     (if (null? L1)
@@ -9,6 +12,5 @@
         (if (null? L2)
             '()
             (cons ( F (car L1) (car L2) ) (zip F (cdr L1) (cdr L2) ) ) ))))
-
 
 ;; Pruebas :
