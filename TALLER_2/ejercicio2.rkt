bf33e6db1d3a441207a99871e6b1017fe9dad1aa
TofiTurtle <juan.rodriguez.lozano@correounivalle.edu.co>
2026-03-15 16:09:00 -0500

    3 primeros parser implementados, un pequeño error deja el ultimo termino fuera, creo saber solucion, denme un rato ahora sigo y arreglo XD

--- TALLER_2/ejercicio2.rkt
+++ TALLER_2/ejercicio2.rkt
@@ -72,147 +72,11 @@
      (list 'or-exp l rest)
 )
 
-; --- predicados
-(define (single-lit? exp)
-   (lit-exp? exp)
-)
 
-(define (or-exp? exp)
-     (and
-         (pair? exp)
-         (eq? (car exp) 'or-exp)
-         (lit-exp? (cadr exp))
-         (list-lit? (caddr exp))
-      )
-)
-
-(define (list-lit? exp)
-   (or
-      (single-lit? exp)
-      (or-exp? exp)
-   )
-)
-
-; --- extractores
-(define list-lit->l
-     (lambda (list-lit)
-          (cadr list-lit)
-     )
-)
-
-; or-exp
-
-; --- extractores, solo estos porque los demas ya estan en list-lit
-(define or-exp->l
-   (lambda (or-exp)
-      (cadr or-exp)
-   )
-)
-
-(define or-exp->rest
-   (lambda (or-exp)
-      (caddr or-exp)
-   )
-)
-
-; clausula
-
-; --- constructores
-(define (make-clause lits)
-   (list 'clause lits)
-)
-
-; --- predicados
-(define (clause? exp)
-   (and
-      (pair? exp)
-      (eq? (car exp) 'clause)
-      (list-lit? (cadr exp))
-   )
-)
-
-; --- extractores
-(define clause->lits
-   (lambda (clause)
-      (cadr clause)
-   )
-)
-
-; list-clausula
-
-; --- constructores
-; no hacemos el de single-cl porque es el mismo que el de clausula, ya que un single-cl es una clausula :3
-
-(define (make-and-exp c rest)
-   (list 'and-exp c rest)
-)
-
-; --- predicados
-(define (single-cl? exp)
-   (clause? exp)
-)
-
-(define (and-exp? exp)
-   (and
-      (pair? exp)
-      (eq? (car exp) 'and-exp)
-      (clause? (cadr exp))
-      (list-clausula? (caddr exp))
-   )
-)
-
-(define (list-clausula? exp)
-  (or
-     (clause? exp)
-     (and-exp? exp)))
-
-; --- extractores
-(define and-exp->c
-   (lambda (and-exp)
-      (cadr and-exp)
-   )
-)
-
-(define and-exp->rest
-   (lambda (and-exp)
-      (caddr and-exp)
-   )
-)
-
-; fnc
-
-; --- constructores
-(define (make-formula n cls)
-   (list 'formula n cls)
-)
-
-; --- predicados
-(define (formula? exp)
-   (and
-      (pair? exp)
-      (eq? (car exp) 'formula)
-      (number? (cadr exp))
-      (list-clausula? (caddr exp))
-   )
-)
-
-; --- extractores
-(define formula->n
-   (lambda (formula)
-      (cadr formula)
-   )
-)
 
 ;;---------------------------------------------------------------
 
 ;; Parser de literal <lit-exp>
-(define (make-pos-lit n)
-     (list 'pos-lit n)
-)
-
-(define (make-neg-lit n)
-     (list 'neg-lit n)
-)
 
 (define parse-lit
   (lambda (dato)
@@ -222,23 +86,18 @@
 
 
 ;; Parser de lista de literales <list-lit>
-(define parse-listlit ???)
 
+(define parse-listlit
+  (lambda (dato)
+    (cond
+      [ (number? dato) (parse-lit dato)]
+      [ (eqv? (cadr dato) 'or) (parse-or dato) ] )  )) ;;MANEJAR UN TERCER CASO!
+
+;;[ (eqv? (cadr dato) 'or) (parse-listlit (cdr(parse-or dato)) ) ] )  ))
 
 ;; Parder de Or Expresion <or-exp>
-(define (make-or-exp l rest)
-     (list 'or-exp l rest)
-)
-
 (define parse-or
   (lambda (dato)
     (make-or-exp
      (parse-lit (car dato))
-     (parse-listlit (caddr dato)) )  ))
-
-
-(define parse-clausula ???)
-(define parse-and ???)
-(define parse-listclausula ???)
-
-(define PARSEBNF ???)
+     (parse-listlit (caddr dato)) )  )) ;;CACMBIAR ESTO!
