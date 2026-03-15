86596b4ffe5fa2b2249a0f6deefbbdab4d2314f4
TofiTurtle <juan.rodriguez.lozano@correounivalle.edu.co>
2026-03-15 14:05:04 -0500

    definidos los parse necesarios xd

--- TALLER_2/ejercicio2.rkt
+++ TALLER_2/ejercicio2.rkt
@@ -1,1 +1,35 @@
 #lang eopl
+
+
+;; terminal -> se lee directo
+;; NO terminal -> Requiere su propio PARSER.
+;;
+;; Gramatica:
+;;
+;     <lit-exp>   ::=  <int>
+;                 ::= -<int>
+
+;     <or-exp>    ::= <lit-exp> or <list-lit>
+
+;     <list-lit>  ::= <lit-exp>
+;                 ::= (or-exp)
+
+;     <clausula>  ::= (<list-lit>)
+
+;     <and-exp>   ::= <clausula> and <list-clausula>
+
+; <list-clausula> ::= <clausula>
+;                 ::= <and-exp>
+
+
+(define PARSEBNF ???)
+
+(define parse-lit
+  (lamda (dato)
+         ???))
+
+(define parse-or ???)
+(define parse-listlit ???)
+(define parse-clausula ???)
+(define parse-and ???)
+(define parse-listclausula ???)
